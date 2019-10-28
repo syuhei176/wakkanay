2538292bed7c147cdd4fe8a592b94bc1f932fcc2
tkmct <1220t.takamichi@gmail.com>
2019-10-28 17:44:12 +0900

    Add failing test to encodable.raw

--- __tests__/Encodables.test.ts
+++ __tests__/Encodables.test.ts
@@ -8,8 +8,8 @@ import {
 } from '../src/types/Encodables'
 
 describe('Encodables', () => {
-  describe('IEncodable.toTypeString()', () => {
-    test('Struct.toTypeString()', () => {
+  describe('IEncodable.toTypeString', () => {
+    test('Struct.toTypeString', () => {
       const v = Struct.from({
         amount: Integer.from(1),
         to: Address.from('0x0472ec0185ebb8202f3d4ddb0226998889663cf2')
@@ -17,7 +17,7 @@ describe('Encodables', () => {
       expect(v.toTypeString()).toBe('Struct<{amount:Integer,to:Address}>')
     })
 
-    test('List.toTypeString()', () => {
+    test('List.toTypeString', () => {
       const v = List.from([
         Struct.from({
           address: Address.from('0x0472ec0185ebb8202f3d4ddb0226998889663cf2')
@@ -30,25 +30,25 @@ describe('Encodables', () => {
     })
   })
 
-  describe('IEncodable.toString()', () => {
-    test('Integer.toString()', () => {
+  describe('IEncodable.toString', () => {
+    test('Integer.toString', () => {
       const v = Integer.from(1)
       expect(v.toString()).toBe('Integer(1)')
     })
 
-    test('Bytes.toString()', () => {
+    test('Bytes.toString', () => {
       const v = Bytes.from(new Uint8Array([0, 1, 2, 3]))
       expect(v.toString()).toBe('Bytes([0,1,2,3])')
     })
 
-    test('Address.toString()', () => {
+    test('Address.toString', () => {
       const v = Address.from('0x0472ec0185ebb8202f3d4ddb0226998889663cf2')
       expect(v.toString()).toBe(
         'Address(0x0472ec0185ebb8202f3d4ddb0226998889663cf2)'
       )
     })
 
-    test('List.toString()', () => {
+    test('List.toString', () => {
       const v = List.from([Integer.from(1), Integer.from(2)])
       expect(v.toString()).toBe('List<Integer>([Integer(1),Integer(2)])')
     })
@@ -67,7 +67,7 @@ describe('Encodables', () => {
       )
     })
 
-    test('Tuple.toString()', () => {
+    test('Tuple.toString', () => {
       const v = Tuple.from([
         Integer.from(1),
         Address.from('0x0472ec0185ebb8202f3d4ddb0226998889663cf2')
@@ -77,7 +77,7 @@ describe('Encodables', () => {
       )
     })
 
-    test('Struct.toString()', () => {
+    test('Struct.toString', () => {
       const v = Struct.from({
         amount: Integer.from(1),
         to: Address.from('0x0472ec0185ebb8202f3d4ddb0226998889663cf2')
@@ -85,6 +85,47 @@ describe('Encodables', () => {
       expect(v.toString()).toBe(
         `Struct({amount:Integer(1),to:Address(0x0472ec0185ebb8202f3d4ddb0226998889663cf2)})`
       )
+    })
+  })
+
+  describe('IEncodable.raw()', () => {
+    test('Integer.raw', () => {
+      const v = Integer.from(99)
+      expect(v.raw).toBe(99)
+    })
+
+    test('Bytes.raw', () => {
+      const v = Bytes.from(new Uint8Array([0, 1, 2, 3]))
+      expect(v.raw).toStrictEqual(new Uint8Array([0, 1, 2, 3]))
+    })
+
+    test('Address.raw', () => {
+      const v = Address.from('0x0472ec0185ebb8202f3d4ddb0226998889663cf2')
+      expect(v.raw).toBe('0x0472ec0185ebb8202f3d4ddb0226998889663cf2')
+    })
+
+    test('List.raw', () => {
+      const v = List.from([Integer.from(1), Integer.from(2)])
+      expect(v.raw).toBe([1, 2])
+    })
+
+    test('Tuple.raw', () => {
+      const v = Tuple.from([
+        Integer.from(1),
+        Address.from('0x0472ec0185ebb8202f3d4ddb0226998889663cf2')
+      ])
+      expect(v.raw).toBe([1, '0x0472ec0185ebb8202f3d4ddb0226998889663cf2'])
+    })
+
+    test('Struct.raw', () => {
+      const v = Struct.from({
+        amount: Integer.from(1),
+        to: Address.from('0x0472ec0185ebb8202f3d4ddb0226998889663cf2')
+      })
+      expect(v.raw).toBe({
+        amount: 1,
+        to: '0x0472ec0185ebb8202f3d4ddb0226998889663cf2'
+      })
     })
   })
 })
