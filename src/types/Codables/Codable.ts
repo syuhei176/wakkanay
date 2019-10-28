94f0ebd13cb0bbc00ed588a203a120959ba49ea2
tkmct <1220t.takamichi@gmail.com>
2019-10-28 18:35:54 +0900

    Remove comment out

--- src/types/Codables/Codable.ts
+++ src/types/Codables/Codable.ts
@@ -1,20 +1,10 @@
 import { ICoder } from '../../coder/ICoder'
 
-// TODO: change this to Codable
-// add decode to interface
 /**
  * Codable abstract class
  * All types that can be encoded to certain encoding format must implement
  * this interface.
  */
-// export default interface Codable {
-//   encode(coder: ICoder): string
-//   decode<T>(coder: ICoder, input: string): T
-//   raw: any
-//   toTypeString(): string
-//   toString(): string
-// }
-
 export default abstract class Codable {
   decode(): Codable {
     throw new Error('Not implemented')
