29b4d7d076e9f8718fef6e657f6499d1afb1d5ed
Takamichi Tsutsumi <1220t.takamichi@gmail.com>
2019-10-29 16:41:55 +0900

    Add todo tests

--- __tests__/coder/EthCoder.test.ts
+++ __tests__/coder/EthCoder.test.ts
@@ -1,9 +1,44 @@
 import EthCoder, { getEthTypeStringRep } from '../../src/coder/EthCoder'
 import Coder from '../../src/coder/Coder'
+import {
+  Codable,
+  Address,
+  Bytes,
+  Integer,
+  List,
+  Tuple,
+  Struct
+} from '../../src/types/Codables'
 
 describe('EthCoder', () => {
   describe('EncodeParameter', () => {
     test.todo('encodeParameter Integer')
+    test.todo('encodeParameter Bytes')
+    test.todo('encodeParameter Address')
+    test.todo('encodeParameter List')
+    test.todo('encodeParameter Tuple')
+    test.todo('encodeParameter Struct')
+  })
+
+  describe('EncodeParameters', () => {
+    test.todo('encodeParameters 1')
+    test.todo('encodeParameters 2')
+    test.todo('encodeParameters 3')
+  })
+
+  describe('DecodeParameter', () => {
+    test.todo('decodeParameter Integer')
+    test.todo('decodeParameter Bytes')
+    test.todo('decodeParameter Address')
+    test.todo('decodeParameter List')
+    test.todo('decodeParameter Tuple')
+    test.todo('decodeParameter Struct')
+  })
+
+  describe('DecodeParameters', () => {
+    test.todo('decodeParameters 1')
+    test.todo('decodeParameters 2')
+    test.todo('decodeParameters 3')
   })
 
   describe('getEthTypeStringRep()', () => {
