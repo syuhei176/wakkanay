3b503bb0011a0db8008b139bdf925fd0fb2a7518
SHUHEI HIYA <syuhei176@gmail.com>
2019-10-30 14:11:56 +0900

    Clean test #19

--- __tests__/ovm/NotDecider.test.ts
+++ __tests__/ovm/NotDecider.test.ts
@@ -3,27 +3,33 @@ import { NotDecider, SampleDecider } from '../../src/ovm/deciders'
 import { Property } from '../../src/ovm/types'
 
 describe('NotDecider', () => {
+  const SampleDeciderAddress = '1'
+  const NotDeciderAddress = '2'
   it('decide not(false)', async () => {
     const deciderManager = new DeciderManager()
-    deciderManager.setDecider('1', new SampleDecider())
-    deciderManager.setDecider('2', new NotDecider())
+    deciderManager.setDecider(SampleDeciderAddress, new SampleDecider())
+    deciderManager.setDecider(NotDeciderAddress, new NotDecider())
     const decision = await deciderManager.decide(
-      new Property('2', [new Property('1', []).encode()])
+      new Property(NotDeciderAddress, [
+        new Property(SampleDeciderAddress, []).encode()
+      ])
     )
     expect(decision.outcome).toEqual(true)
   })
   it('decide not(true)', async () => {
     const deciderManager = new DeciderManager()
-    deciderManager.setDecider('1', new SampleDecider())
-    deciderManager.setDecider('2', new NotDecider())
+    deciderManager.setDecider(SampleDeciderAddress, new SampleDecider())
+    deciderManager.setDecider(NotDeciderAddress, new NotDecider())
     const decision = await deciderManager.decide(
-      new Property('2', [new Property('1', ['true']).encode()])
+      new Property(NotDeciderAddress, [
+        new Property(SampleDeciderAddress, ['true']).encode()
+      ])
     )
     expect(decision.outcome).toEqual(false)
     // valid challenge is SampleDecider(true)
     expect(decision.challenges[0]).toEqual({
       challengeInput: null,
-      property: { deciderAddress: '1', inputs: ['true'] }
+      property: { deciderAddress: SampleDeciderAddress, inputs: ['true'] }
     })
   })
 })
