d0083b92a7ab8032702bf292afe4a1e9ddcfd3ac
Takamichi Tsutsumi <1220t.takamichi@gmail.com>
2019-11-07 16:13:16 +0900

    Add IsHashPreimageDecider

---
+++ __tests__/ovm/IsHashPreimageDecider.test.ts
@@ -0,0 +1,38 @@
+import { DeciderManager } from '../../src/ovm/DeciderManager'
+import { IsHashPreimageDecider } from '../../src/ovm/deciders'
+import { Property } from '../../src/ovm/types'
+import { Address, Bytes } from '../../src/types/Codables'
+import { keccak256 } from 'ethers/utils'
+
+describe('IsHashPreimageDecider', () => {
+  const addr = Address.from('0x0000000000000000000000000000000000000001')
+  const deciderManager = new DeciderManager()
+  deciderManager.setDecider(addr, new IsHashPreimageDecider())
+
+  const preimage = Bytes.fromString('plasma is awesome!!')
+  const hash = Bytes.fromString(keccak256(preimage.data))
+
+  test('valid hash preimage', async () => {
+    const property = new Property(addr, [hash, preimage])
+
+    const decision = await deciderManager.decide(property)
+    expect(decision.outcome).toBeTruthy()
+  })
+
+  test('invalid hash preimage', async () => {
+    const property = new Property(addr, [
+      hash,
+      Bytes.fromString('falsey preimage')
+    ])
+
+    const decision = await deciderManager.decide(property)
+    expect(decision.outcome).toBeFalsy()
+  })
+
+  test('input tuple length is invalid', async () => {
+    const property = new Property(addr, [hash])
+
+    const decision = await deciderManager.decide(property)
+    expect(decision.outcome).toBeFalsy()
+  })
+})
--- src/ovm/deciders/index.ts
+++ src/ovm/deciders/index.ts
@@ -1,3 +1,4 @@
 export * from './atomic/SampleDecider'
+export * from './atomic/IsHashPreimageDecider'
 export * from './operators/AndDecider'
 export * from './operators/NotDecider'
