e429efb94fc1286acac6f63163a01a2825cb2f03
daiki.sekiguchi <sekiseki0213@gmail.com>
2019-11-18 12:53:43 +0900

    Fix readonly

--- src/types/balance.ts
+++ src/types/balance.ts
@@ -1,30 +1,11 @@
 import BigNumber from 'bignumber.js'
 
 export default class Balance {
-  private _value: BigNumber
-  private _decimals: number
-  private _symbol: string
-  constructor(value: BigNumber, decimals: number, symbol: string) {
-    this._value = value
-    this._decimals = decimals
-    this._symbol = symbol
-  }
-
-  public get balance(): Balance {
-    return this
-  }
-
-  public get value(): BigNumber {
-    return this._value
-  }
-
-  public get decimals(): number {
-    return this._decimals
-  }
-
-  public get symbol(): string {
-    return this._symbol
-  }
+  constructor(
+    readonly value: BigNumber,
+    readonly decimals: number,
+    readonly symbol: string
+  ) {}
 
   public get displayValue(): string {
     return `${this.value.dividedBy(new BigNumber(10 ** this.decimals))} ${
