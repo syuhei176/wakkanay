88049bb64d61f664cecfdc7044767e42f90bd0aa
SHUHEI HIYA <syuhei176@gmail.com>
2019-11-21 17:48:41 +0900

    Clean verifier test code

--- __tests__/verifiers/Ed25519.test.ts
+++ __tests__/verifiers/Ed25519.test.ts
@@ -1,79 +1,18 @@
 import { ed25519Verifier } from '../../src/verifiers'
 import { Bytes } from '../../src/types/Codables'
-import sodiumsumo from 'libsodium-wrappers-sumo'
-import base58check from 'bs58check'
-
-async function sign(msg: Buffer, key: any) {
-  await sodiumsumo.ready
-  const b = sodiumsumo.crypto_sign_detached(msg, key)
-  return Buffer.from(b)
-}
-
-// utility function for tezos key management
-function readPublicKey(hex: string): string {
-  if (hex.length !== 66 && hex.length !== 68) {
-    throw new Error(`Incorrect hex length, ${hex.length} to parse a key`)
-  }
-
-  let hint = hex.substring(0, 2)
-  if (hint === '00') {
-    // ed25519
-    return base58check.encode(Buffer.from('0d0f25d9' + hex.substring(2), 'hex'))
-  } else if (hint === '01' && hex.length === 68) {
-    // secp256k1
-    return base58check.encode(Buffer.from('03fee256' + hex.substring(2), 'hex'))
-  } else if (hint === '02' && hex.length === 68) {
-    // p256
-    return base58check.encode(Buffer.from('03b28b7f' + hex.substring(2), 'hex'))
-  } else {
-    throw new Error('Unrecognized key type')
-  }
-}
-
-// utility function for tezos key management
-async function restoreKey(
-  privateKey: string
-): Promise<{ publicKey: string; privateKey: string }> {
-  await sodiumsumo.ready
-
-  const secretKey = base58check.decode(privateKey).slice(4)
-  const seed = sodiumsumo.crypto_sign_ed25519_sk_to_seed(secretKey)
-  // @ts-ignore
-  const keys = sodiumsumo.crypto_sign_seed_keypair(seed, '')
-  const publicKey = readPublicKey(
-    `00${Buffer.from(keys.publicKey).toString('hex')}`
-  )
-
-  return { publicKey, privateKey }
-}
 
 describe('ed25519Verifier', () => {
-  const privateKey =
-    'edskRpVqFG2FHo11aB9pzbnHBiPBWhNWdwtNyQSfEEhDf5jhFbAtNS41vg9as7LSYZv6rEbtJTwyyEg9cNDdcAkSr9Z7hfvquB'
-  const anotherPrivateKey =
-    'edskS5pf29PwnxHN7P7UhyR5t8mKsh3CiH86mAKZZ9E3Y46d1AULDk4N9CxugkeD5UAGvL7UyXVFRptSy439YT1jvMoGA8GMoR'
-  let publicKey: Bytes
+  const publicKey = Bytes.fromHexString(
+    '0x6564706b7575474a34737348334e356b376f76776b42653136703872565831584c454e695a344641617972637755663973434b586e47'
+  )
   const message = Bytes.fromString('message')
-  let signature: Bytes
-  let invalidSignature: Bytes
-  beforeEach(async () => {
-    await sodiumsumo.ready
-
-    const keyStore = await restoreKey(privateKey)
-    publicKey = Bytes.fromString(keyStore.publicKey)
-    const messageBuffer = Buffer.from(message.toHexString())
-    const privateKeyBuffer = base58check.decode(privateKey).slice(4)
-    const anotherPrivateKeyBuffer = base58check
-      .decode(anotherPrivateKey)
-      .slice(4)
-    const signatureBuffer = await sign(messageBuffer, privateKeyBuffer)
-    const invalidSignatureBuffer = await sign(
-      messageBuffer,
-      anotherPrivateKeyBuffer
-    )
-    signature = Bytes.from(signatureBuffer)
-    invalidSignature = Bytes.from(invalidSignatureBuffer)
-  })
+  const signature = Bytes.fromHexString(
+    '0x677292276737c789d826e6e46fbcca4c768da3992074a68ab13d9e25e112c2075685c7a974fe0ab875bc8ac98f1cb3f2b0e053785f07ba608298b9e3389cf404'
+  )
+  const invalidSignature = Bytes.fromHexString(
+    '0x011e58abd9fd95dfd8535f8aecc54a0dbd0cba924b0ee964487436e5ec3a38e16d174470801904d00245455e22f195685da02152f677ef8df78c931b494a220f'
+  )
+  const emptySignature = Bytes.default()
 
   it('return true with valid signature', async () => {
     const verify = await ed25519Verifier.verify(message, signature, publicKey)
@@ -87,5 +26,11 @@ describe('ed25519Verifier', () => {
       publicKey
     )
     expect(verify).toBeFalsy()
+  })
+
+  it('throw exception with empty signature', async () => {
+    await expect(
+      ed25519Verifier.verify(message, emptySignature, publicKey)
+    ).rejects.toEqual(new Error('invalid signature length'))
   })
 })
