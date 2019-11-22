e41c1245d63581aa02b4f1761efc51aa92906af1
SHUHEI HIYA <syuhei176@gmail.com>
2019-11-22 09:10:39 +0900

    feat(types): Add Range data structure #16

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,24 @@
+import { Integer, Struct } from './Codables'
+
 export type ParamType = {
   name?: string
   type: string
   indexed?: boolean
   components?: Array<any>
+}
+
+export class Range {
+  constructor(private start: Integer, private end: Integer) {}
+  public toStruct(): Struct {
+    return new Struct({
+      start: this.start,
+      end: this.end
+    })
+  }
+  public static fromStruct(_struct: Struct): Range {
+    return new Range(
+      _struct.data['start'] as Integer,
+      _struct.data['end'] as Integer
+    )
+  }
 }
