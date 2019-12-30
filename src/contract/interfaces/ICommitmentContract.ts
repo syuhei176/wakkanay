07255332a5e6f3af603b0406fd166f6940238a38
tkmct <1220t.takamichi@gmail.com>
2019-12-31 00:42:16 +0900

    feat(contract) add getCurrentBlock to ICommitmentContract

--- src/contract/interfaces/ICommitmentContract.ts
+++ src/contract/interfaces/ICommitmentContract.ts
@@ -3,6 +3,8 @@ import { Bytes, BigNumber } from '../../types/Codables'
 export interface ICommitmentContract {
   submit(blockNumber: BigNumber, root: Bytes): Promise<void>
 
+  getCurrentBlock(): Promise<BigNumber>
+
   subscribeBlockSubmitted(
     handler: (blockNumber: BigNumber, root: Bytes) => void
   ): void
