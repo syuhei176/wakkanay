a4b1a1487a5d2b6d30982dcfbacec805a79bda6b
tkmct <1220t.takamichi@gmail.com>
2020-01-04 16:25:25 +0900

    fix Range test

--- src/types/types.ts
+++ src/types/types.ts
@@ -10,21 +10,27 @@ export type ParamType = {
 export class Range {
   constructor(readonly start: BigNumber, readonly end: BigNumber) {}
   public toStruct(): Struct {
-    return new Struct({
-      start: this.start,
-      end: this.end
-    })
+    return new Struct([
+      {
+        key: 'start',
+        value: this.start
+      },
+      { key: 'end', value: this.end }
+    ])
   }
   public static fromStruct(_struct: Struct): Range {
     return new Range(
-      _struct.data['start'] as BigNumber,
-      _struct.data['end'] as BigNumber
+      _struct.data[0].value as BigNumber,
+      _struct.data[1].value as BigNumber
     )
   }
   public static getParamType(): Struct {
-    return Struct.from({
-      start: BigNumber.default(),
-      end: BigNumber.default()
-    })
+    return Struct.from([
+      {
+        key: 'start',
+        value: BigNumber.default()
+      },
+      { key: 'end', value: BigNumber.default() }
+    ])
   }
 }
