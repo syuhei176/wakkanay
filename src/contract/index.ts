0320e9cb3d81060991fe57d83d329acd56e1926d
Shuhei Hiya <syuhei176@gmail.com>
2020-01-09 23:16:11 +0900

    feat: add the interface of adjudication contract #9

--- src/contract/index.ts
+++ src/contract/index.ts
@@ -1,3 +1,4 @@
+export * from './interfaces/IAdjudicationContract'
 export * from './interfaces/ICommitmentContract'
 export * from './interfaces/IDepositContract'
 export * from './interfaces/IERC20Contract'
