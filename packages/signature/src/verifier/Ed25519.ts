529c3e0c1b5065cbcb9899d1b1da6abd411953ad
Shuhei Hiya <syuhei176@gmail.com>
2020-03-04 09:33:12 +0900

    clean(verifier): clean ed25519Verifier

--- packages/signature/src/verifier/Ed25519.ts
+++ packages/signature/src/verifier/Ed25519.ts
@@ -5,12 +5,11 @@ import { Bytes } from '@cryptoeconomicslab/primitives'
 export const ed25519Verifier: SignatureVerifier = {
   verify: async (message: Bytes, signature: Bytes, publicKey: Bytes) => {
     const publicKeyHeaderAndBody = publicKey.split(1)
-    const sig = Buffer.from(signature.toHexString().substr(2), 'hex')
-    const msg = Buffer.from(message.toHexString().substr(2), 'hex')
-    const pk = Buffer.from(
-      publicKeyHeaderAndBody[1].toHexString().substr(2),
-      'hex'
-    )
+    const [sig, msg, pk] = [
+      signature,
+      message,
+      publicKeyHeaderAndBody[1]
+    ].map(b => Buffer.from(b.toHexString().substr(2), 'hex'))
     return tweetnacl.sign.detached.verify(msg, sig, pk)
   }
 }
