555b4e2d555d8e53fd2dc1dae9d7c6a4b2a8cdba
tkmct <1220t.takamichi@gmail.com>
2020-05-04 14:50:19 +0900

    extract token related contracts method to tokenManager

--- packages/plasma-light-client/src/managers/TokenManager.ts
+++ packages/plasma-light-client/src/managers/TokenManager.ts
@@ -1,15 +1,63 @@
-import { IERC20DetailedContract } from '@cryptoeconomicslab/contract'
+import {
+  IERC20DetailedContract,
+  IDepositContract
+} from '@cryptoeconomicslab/contract'
 import { Integer, Address } from '@cryptoeconomicslab/primitives'
 
 /**
- * @description TokenManager manages token information
+ * @description TokenManager manages token contracts and deposit contracts
  */
 export default class TokenManager {
+  private depositContractAddressStrings: Set<string> = new Set()
+  private depositContracts: Map<string, IDepositContract> = new Map()
   private tokenContracts: Map<string, IERC20DetailedContract> = new Map()
   private tokenDecimals: Map<string, Integer> = new Map()
 
+  get depositContractAddresses(): Address[] {
+    return Array.from(this.depositContractAddressStrings).map(addr =>
+      Address.from(addr)
+    )
+  }
+
   /**
-   * Add Token Contract by Deposit Contract address.
+   * add pair of ERC20 contract and Deposit contract to be managed
+   * @param erc20Contract ERC20 contract to add
+   * @param depositContract Deposit contract to add
+   */
+  async addContracts(
+    erc20Contract: IERC20DetailedContract,
+    depositContract: IDepositContract
+  ) {
+    const depositContractAddress = depositContract.address
+    this.addDepositContract(depositContractAddress, depositContract)
+    await this.addTokenContract(depositContractAddress, erc20Contract)
+  }
+
+  /**
+   * Add Deposit Contract with key of Deposit Contract's address.
+   * @param depositContractAddress address of deposit contract
+   * @param depositContract Instance of DepositContract
+   */
+  addDepositContract(
+    depositContractAddress: Address,
+    depositContract: IDepositContract
+  ) {
+    this.depositContracts.set(depositContractAddress.data, depositContract)
+    this.depositContractAddressStrings.add(depositContractAddress.data)
+  }
+
+  /**
+   * get deposit contract by address
+   * @param depositContractAddress address of deposit contract
+   */
+  getDepositContract(
+    depositContractAddress: Address
+  ): IDepositContract | undefined {
+    return this.depositContracts.get(depositContractAddress.data)
+  }
+
+  /**
+   * Add Token Contract with key of Deposit Contract's address.
    * @param depositContractAddress The address of Deposit Contract
    * @param erc20Contract Instance of ERC20Contract
    */
