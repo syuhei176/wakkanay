0f32c44afbe8abbc5268f4af65532c755ccfe9d7
daiki.sekiguchi <daiki.skgc44@gmail.com>
2020-05-22 00:57:53 +0900

    fix plasma-light-client

--- packages/plasma-light-client/src/LightClient.ts
+++ packages/plasma-light-client/src/LightClient.ts
@@ -229,8 +229,13 @@ export default class LightClient {
   > {
     const resultPromise = this.tokenManager.tokenContractAddresses.map(
       async addr => {
+        const depositContractAddress = this.tokenManager.getDepositContractAddress(
+          addr
+        )
+        if (!depositContractAddress)
+          throw new Error('Deposit Contract Address not found')
         const data = await this.stateManager.getVerifiedStateUpdates(
-          addr,
+          Address.from(depositContractAddress),
           new Range(BigNumber.from(0), BigNumber.MAX_NUMBER) // TODO: get all stateUpdate method
         )
         const tokenContract = this.tokenManager.getTokenContract(addr)
@@ -321,9 +326,7 @@ export default class LightClient {
     )
     const storageDb = await this.deciderManager.getStorageDb()
     const bucket = await storageDb.bucket(
-      Bytes.fromHexString(
-        this.deciderConfig.constantVariableTable.commitmentContract
-      )
+      Bytes.fromHexString(this.deciderConfig.commitmentContract)
     )
     await bucket.put(coder.encode(blockNumber), coder.encode(root))
 
@@ -724,12 +727,6 @@ export default class LightClient {
     if (!depositContractAddress) {
       throw new Error('Deposit Contract Address not found')
     }
-    const depositContract = this.tokenManager.getDepositContract(
-      Address.from(depositContractAddress)
-    )
-    if (!depositContract) {
-      throw new Error('Deposit Contract not found')
-    }
     const stateUpdates = await this.stateManager.resolveStateUpdate(
       Address.from(depositContractAddress),
       amount
@@ -907,14 +904,8 @@ export default class LightClient {
     if (!depositContractAddress) {
       throw new Error('Deposit Contract Address not found')
     }
-    const depositContract = this.tokenManager.getDepositContract(
-      Address.from(depositContractAddress)
-    )
-    if (!depositContract) {
-      throw new Error('Deposit Contract not found')
-    }
     const stateUpdates = await this.stateManager.resolveStateUpdate(
-      depositContract.address,
+      Address.from(depositContractAddress),
       amount
     )
     if (Array.isArray(stateUpdates) && stateUpdates.length > 0) {
